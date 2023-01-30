43cb3079bf47836ce18de49d4792d74e10fba87a
R. Savero <116779928+vero8090@users.noreply.github.com>
2023-01-30 17:32:38 +0700

    Add files via upload

---
+++ execution/App.js
@@ -0,0 +1,11 @@
+// import Test from './pages/test' ini cek ongkir
+import BusSeat from "./pages/busSeat"
+export default function App(){
+
+  return(
+    <>
+     {/* <Test/> */}
+     <BusSeat/>
+    </>
+  )
+}
